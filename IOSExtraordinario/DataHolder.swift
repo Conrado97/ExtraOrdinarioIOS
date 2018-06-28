b9a161928dd9782202005420a26e6edbe04eed90
Conrado97 <cdelsogonzalez@gmail.com>
2018-06-28 13:13:53 +0200

    cosas, dataholder, registrar creadas

---
+++ IOSExtraordinario/DataHolder.swift
@@ -0,0 +1,9 @@
+//
+//  DataHolder.swift
+//  IOSExtraordinario
+//
+//  Created by CONRADO DELSO GONZALEZ on 28/6/18.
+//  Copyright © 2018 CONRADO DELSO GONZALEZ. All rights reserved.
+//
+
+import Foundation
